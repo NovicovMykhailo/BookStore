4b748030cdf4ab53ac3a508ecc3344fe814be895
HalynaSilina <halynasilina@gmail.com>
2023-04-12 19:00:27 +0300

    add book card template function and basic styles

---
+++ src/js/book-card-template.js
@@ -0,0 +1,10 @@
+export function createBookCard(book) {
+  const { book_image, title, author } = book;
+  return `<div class="book-card">
+    <img class="book-card__image" src="${book_image}", alt="${author} ${title}" loading="lazy" />
+    <div class="book-card__info">
+    <p class="info-item__title">${title}</p>
+    <p class="info-item__author">${author}</p>
+    </div>
+    </div>`;
+}
