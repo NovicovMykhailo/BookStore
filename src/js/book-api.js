d78ff1d72af9a3c3847015d6cf8e74463f8aff95
HalynaSilina <halynasilina@gmail.com>
2023-04-12 11:03:51 +0300

    Update book-api.js
    Change class methods by using axios library

--- src/js/book-api.js
+++ src/js/book-api.js
@@ -1,67 +1,30 @@
+import axios from 'axios';
+
 export class BookAPI {
   #BASE_URL = 'https://books-backend.p.goit.global/books';
 
+  constructor() {
+    this.category = '';
+    this.id = '';
+  }
+
   //   Метод для отримання списку категорій (повертає масив з назвами категорій)
-  getBookCategoriesList() {
-    return fetch(`${this.#BASE_URL}/category-list`)
-      .then(res => {
-        if (!res.ok) {
-          throw new Error(res.status);
-        }
-        return res.json();
-      })
-      .then(data => data.map(category => category.list_name));
+  getBooksCategoriesList() {
+    return axios.get(`${this.#BASE_URL}/category-list`);
   }
 
   //   Метод для отримання списку бестселлерів по категоріях (повертає масив)
   getTopBooks() {
-    return fetch(`${this.#BASE_URL}/top-books`)
-      .then(res => {
-        if (!res.ok) {
-          throw new Error(res.status);
-        }
-        return res.json();
-      })
-      .then(data => {
-        const category = data.list_name;
-        const topBooks = data.books.map(book => ({
-          bookID: book._id,
-          image: book.book_image,
-          author: book.author,
-          title: book.title,
-        }));
-        return category, topBooks;
-      });
+    return axios.get(`${this.#BASE_URL}/top-books`);
   }
 
   //   Метод для отримання масиву книг з обраної категорії
-  getSelectedCategoryBooks(selectedCategory) {
-    return fetch(`${this.#BASE_URL}/category?category=${selectedCategory}`)
-      .then(res => {
-        if (!res.ok) {
-          throw new Error(res.status);
-        }
-        return res.json();
-      })
-      .then(data =>
-        data.books.map(book => ({
-          bookID: book._id,
-          image: book.book_image,
-          author: book.author,
-          title: book.title,
-        }))
-      );
+  getSelectedCategoryBooks() {
+    return axios.get(`${this.#BASE_URL}/category?category=${this.category}`);
   }
 
   //   Метод для отримання інформації про книгу за її унікальним ідентифікатором
-  getBookInfo(bookID) {
-    return fetch(`${this.#BASE_URL}/${bookID}`)
-      .then(res => {
-        if (!res.ok) {
-          throw new Error(res.status);
-        }
-        return res.json();
-      })
-      .then(data => ({ _id, list_name, author, title, book_image, description, buy_links } = data));
+  getBookInfo() {
+    return axios.get(`${this.#BASE_URL}/${this.id}`);
   }
 }
