9de28c1111aba5585ac1ac18d90fb2866389475c
Vika Maletskaya <v.maletskayaah@gmail.com>
2023-04-13 15:02:47 +0300

    update function renderBooksList

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 
 import './js/scroll-top-btn.js'
+import './js/top-book.js'
--- src/js/top-book.js
+++ src/js/top-book.js
@@ -1,3 +1,4 @@
+
 import { BookAPI } from './book-api.js';
 import { createBookCard } from './book-card-template.js';
 
@@ -5,17 +6,49 @@ const bookAPI = new BookAPI();
 
 async function renderBooksList() {
   const booksContainer = document.createElement('books-container');
-  //очищение контейнера перед загрузкой новых данных
-    
+
+   //очищение контейнера перед загрузкой новых данных
+  
   booksContainer.innerHTML = '';
 
   try {
+    // получение списка категорий
+    const categories = await bookAPI.getBooksCategoriesList();
+
     // получение бестселлеров
-      
-      const topBooks = await bookAPI.getTopBooks();
-      
-     // создание списка бестселлеров
-      
+    const topBooks = await bookAPI.getTopBooks();
+
+    
+    for (const category of categories.data) {
+      // получение книги с категории
+      bookAPI.category = category;
+      const books = await bookAPI.getSelectedCategoryBooks();
+
+      // создание заголовка категории
+      const categoryTitle = document.createElement('h2');
+      categoryTitle.className = 'category-text'
+      categoryTitle.innerText = category;
+      booksContainer.appendChild(categoryTitle);
+
+      // создание списка книг
+      const booksList = document.createElement('ul');
+      booksList.className = 'books-list';
+      for (const book of books.data) {
+        const bookCard = createBookCard(book);
+        const bookListItem = document.createElement('li');
+        bookListItem.innerHTML = bookCard;
+        booksList.appendChild(bookListItem);
+      }
+      booksContainer.appendChild(booksList);
+    }
+
+    // создание заголовка для бестселлера
+    const topBooksTitle = document.createElement('h2');
+    topBooksTitle.className = 'section-title'
+    booksContainer.appendChild(topBooksTitle);
+
+    // создание списка бестселлеров
+
     const topBooksList = document.createElement('ul');
     topBooksList.className = 'books-list';
     for (const book of topBooks.data) {
@@ -28,6 +61,9 @@ async function renderBooksList() {
   } catch (error) {
     console.error(error);
   }
+
+  // const topBooksBtn = document.createElement('button');
+  // topBooksBtn.className = 'btn-default'
 }
 
 // renderBooksList();
