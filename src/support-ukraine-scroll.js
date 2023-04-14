b340aa876504b9dc74dc70f74dadc6ac537a1377
Serhii Naboka <drifterzzz@gmail.com>
2023-04-14 16:08:22 +0300

    asxxa

--- src/support-ukraine-scroll.js
+++ src/support-ukraine-scroll.js
@@ -1,4 +1,33 @@
 const supportCompaniesRef = document.querySelector('.support-companies');
-const seeMoreBtn = document.querySelector('.see-more-btn');
-console.log(supportCompaniesRef);
-console.log(seeMoreBtn);
+const scrollDownBtn = document.querySelector('.scroll-down-btn');
+const arrayOfSupportCompanies = document.querySelectorAll(
+  '.support-companies__item'
+);
+
+// scrollDownBtn.addEventListener('click', onScrollDownBtnClick);
+
+// const arrayOfSupportCompanies = supportCompaniesRef.children;
+
+// function onScrollDownBtnClick() {}
+
+// const children = supportCompaniesRef.childNodes;
+
+// console.log(children);
+
+// supportCompaniesRef.lastElementChild.innerHTML = 'acaccaca';
+
+// const supCompaniesNewArr = [...arrayOfSupportCompanies];
+
+// console.log(supCompaniesNewArr);
+// console.log(supCompaniesNewArr[0]);
+
+// supCompaniesNewArr.forEach(company => {
+//   console.log(company.children[0].src);
+// });
+
+// const saveChImg = document.querySelector('.save-children-img');
+// console.log(saveChImg.src);
+
+arrayOfSupportCompanies.forEach(company => {
+  console.log(company.children[0].src);
+});
