f0b199114e57f4d41b4252288a6a403345f24d05
Serhii Naboka <drifterzzz@gmail.com>
2023-04-14 17:14:18 +0300

    asad

--- src/support-ukraine-scroll.js
+++ src/support-ukraine-scroll.js
@@ -1,33 +1,32 @@
-const supportCompaniesRef = document.querySelector('.support-companies');
+// const supportCompaniesRef = document.querySelector('.support-companies');
 const scrollDownBtn = document.querySelector('.scroll-down-btn');
 const arrayOfSupportCompanies = document.querySelectorAll(
   '.support-companies__item'
 );
 
-// scrollDownBtn.addEventListener('click', onScrollDownBtnClick);
+console.log(arrayOfSupportCompanies);
 
-// const arrayOfSupportCompanies = supportCompaniesRef.children;
-
-// function onScrollDownBtnClick() {}
-
-// const children = supportCompaniesRef.childNodes;
-
-// console.log(children);
-
-// supportCompaniesRef.lastElementChild.innerHTML = 'acaccaca';
-
-// const supCompaniesNewArr = [...arrayOfSupportCompanies];
-
-// console.log(supCompaniesNewArr);
-// console.log(supCompaniesNewArr[0]);
-
-// supCompaniesNewArr.forEach(company => {
-//   console.log(company.children[0].src);
-// });
+scrollDownBtn.addEventListener('click', onScrollDownBtnClick);
 
 // const saveChImg = document.querySelector('.save-children-img');
 // console.log(saveChImg.src);
 
-arrayOfSupportCompanies.forEach(company => {
-  console.log(company.children[0].src);
-});
+// function onScrollDownBtnClick() {
+//   const arrOfCompanyImgSrc = [];
+
+//   arrayOfSupportCompanies.forEach(company => {
+//     arrOfCompanyImgSrc.push(company.children[0].src);
+//   });
+//   console.log(arrOfCompanyImgSrc);
+// }
+
+function onScrollDownBtnClick() {
+  const arrOfCompanyImgSrc = [];
+
+  arrayOfSupportCompanies.forEach(company => {
+    // console.log(company.children[0].src);
+    arrOfCompanyImgSrc.push(company.children[0].src);
+    // company.children[0].src = company.children[0].nextSibling.src;
+  });
+  console.log(arrOfCompanyImgSrc);
+}
