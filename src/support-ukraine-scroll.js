2c9c1e4bed0d82a62718dd38a9ac9cf500476aea
Serhii Naboka <drifterzzz@gmail.com>
2023-04-14 14:55:12 +0300

    adda

---
+++ src/support-ukraine-scroll.js
@@ -0,0 +1,4 @@
+const supportCompaniesRef = document.querySelector('.support-companies');
+const seeMoreBtn = document.querySelector('.see-more-btn');
+console.log(supportCompaniesRef);
+console.log(seeMoreBtn);
