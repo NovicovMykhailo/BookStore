7e4c8df81fd7b867e5996f491bda9a09b84457cd
Phokoos <hulkovskij@gmail.com>
2023-04-14 16:35:54 +0300

    Fixed bags and added some css

--- src/js/filter.js
+++ src/js/filter.js
@@ -5,10 +5,6 @@ import { colorLastWordInTitle } from "./color-last-words";
 const bookApi = new BookAPI();
 
 const filterListEl = document.querySelector(".filter__list");
-// const filterItemAllEl = document.querySelectorAll(".filter__item");
-// console.log(filterListEl);
-
-// console.log("Start");
 
 bookApi.getBooksCategoriesList().then(
 	data => {
@@ -22,15 +18,14 @@ bookApi.getBooksCategoriesList().then(
 let varWithActiveValueFilter = document.querySelector(".filter__item--active");
 let varWithCurrentCategoryValue = "ALL CATEGORIES";
 
-
 filterListEl.addEventListener("click", event => {
 	if (event.target.outerText.toLowerCase() === varWithCurrentCategoryValue.toLowerCase()) {
-		return console.log("Stop");	
+		return 	
 	}
 	varWithCurrentCategoryValue = event.target.outerText;
-	// console.log("currentValue: " + varWithCurrentCategoryValue);
+	
 	const targetEl = event.target;
-	// console.log(varWithActiveValueFilter);
+	
 	varWithActiveValueFilter.classList.remove("filter__item--active");
 	
 	targetEl.classList.add("filter__item--active");
@@ -42,41 +37,20 @@ filterListEl.addEventListener("click", event => {
 })
 
 
-const galleryListEl = document.querySelector(".book-card__list");
+const galleryListEl = document.querySelector(".gallery__list");
 const galleryTitle = document.querySelector(".book-card__title");
-// console.log(galleryListEl);
 
 const fetchToApiUseCatagory = (value) => {
-	// const bookApiCat = new BookAPI();
+	
 	bookApi.category = value;
 	galleryTitle.innerHTML = value;
-	// console.log("Console cat from class: " + bookApi.category);
-	// let galleryItemElems;
-
-	// bookApi.getSelectedCategoryBooks().then(data => {
-	// 	let galleryItemElems = data.data.map(({ book_image, title, author, description}) => {
-	// 		return `<li class="gallery__item">
-	//    				<img
-	// 					class="gallery_img"
-	//     				 src="${book_image}"
-	//     				 alt="${description}"
-	//      				 width="100%"
-	//    				/>
-	//    			  <h2 class="gallery__title">${title}</h2>
-	//    			  <p class="gallery__author">${author}</p>
-	//  				</li>`}).join("");
-	// 	galleryListEl.innerHTML = "";
-	// 	galleryListEl.insertAdjacentHTML("beforeend", galleryItemElems);
-	// })
-
-	console.log(createBookCard({ book_image: "href", title: "title", author: "author" }));
 	
 	bookApi.getSelectedCategoryBooks().then(data => {
 		let galleryItemElems = data.data.map(({ book_image, title, author, description }) => {
 			return createBookCard({ book_image: book_image, title: title, author: author });
-		})
+		}).join("");
 		galleryListEl.innerHTML = "";
 		galleryListEl.insertAdjacentHTML("beforeend", galleryItemElems);
-	}
+		}
 	)
-	}
+}
