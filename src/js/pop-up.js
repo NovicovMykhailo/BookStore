c8b1ebf33fa85f4d96e5fb37d16b6d5e64089a6e
Stanislav-Zhuk <steys.ua@gmail.com>
2023-04-16 13:45:29 +0300

    Update pop-up.js

--- src/js/pop-up.js
+++ src/js/pop-up.js
@@ -51,3 +51,42 @@ bookCards.forEach(bookCard => {
 //     });
 //   });
 // });
+
+// 
+
+// export async function fetchAndRenderModalPopUp() {
+//   try {
+//     const response = await books.forEach(book => {
+//       const bookCard = createBookCard(book);
+//       bookList.appendChild(bookCard);
+
+//       bookCard.addEventListener('click', event => {
+//         event.preventDefault();
+
+//         const bookId = bookCard.querySelector('.book-card__id').textContent;
+//         bookAPI.id = bookId;
+
+//         bookAPI.getBookInfo()
+//           .then(response => {
+//             const book = response.data;
+//             const modalPopUp = createModalPopUpCard(book);
+//             document.body.insertAdjacentHTML('beforeend', modalPopUp);
+//             const modalElement = document.getElementById(book._id);
+
+//             modalElement.style.display = 'block';
+//             modalElement.addEventListener('click', event => {
+//               if (event.target === modalElement || event.target.classList.contains('pop-up__btn-close')) {
+//                 modalElement.style.display = 'none';
+//                 modalElement.remove();
+//               }
+//             });
+//           })
+//           .catch(error => {
+//             console.error(error);
+//           });
+//       });
+//     });
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
