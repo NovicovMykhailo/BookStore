048ac98d2b678e07e5f3e3a6938d3eda182c266d
NovicovMykhailo <mikenovicov@gmail.com>
2023-04-16 17:39:14 +0300

    fix deleted files

--- src/index.js
+++ src/index.js
@@ -2,12 +2,5 @@ import './js/scroll-top-btn.js';
 import './js/top-book.js';
 import './js/filter.js';
 import './js/color-last-words.js';
-import './js/theme-switcher.js';
-<<<<<<< HEAD
 
-import './js/theme-switcher.js'
-import './js/modal-signUp-login'
-=======
-import './js/modal-signUp-login';
 import './js/support-companies.js';
->>>>>>> 65e48da6f6837fe6be45afa743dc1f01da65927d
