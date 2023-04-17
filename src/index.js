aceca96780cd221b1a7c6c75bc2737e2514a0ee9
Roman Kolobych <romankol1223@gmail.com>
2023-04-17 17:11:20 +0300

    modal/window

--- src/index.js
+++ src/index.js
@@ -4,4 +4,5 @@ import './js/filter.js';
 import './js/color-last-words.js';
 import './js/theme-switcher.js';
 import './js/modal-signUp-login';
-import './js/support-companies.js'
+import './js/support-companies.js';
+import './js/mobile-menu';
--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,29 +1,31 @@
 export function MobileMenu() {
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
-  const closeMenuBtn = document.querySelector('.js-close-menu');
+  const menuIcon = document.querySelector('#menu-icon');
 
   const toggleMenu = () => {
-    const isMenuOpen =
-      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    const isMenuOpen = mobileMenu.classList.contains('is-open');
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
 
-    const scrollLockMethod = !isMenuOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
+    if (isMenuOpen) {
+      menuIcon.setAttribute('href', './images/svg-sprite.svg#ham-menu-icon');
+      document.body.classList.remove('no-scroll'); // дозволяємо прокручування
+    } else {
+      menuIcon.setAttribute('href', './images/svg-sprite.svg#x-close-icon');
+      document.body.classList.add('no-scroll'); // блокуємо прокручування
+    }
   };
 
   openMenuBtn.addEventListener('click', toggleMenu);
-  closeMenuBtn.addEventListener('click', toggleMenu);
 
   const mediaQuery = window.matchMedia('(min-width: 768px)');
   const handleOrientationChange = e => {
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open');
     openMenuBtn.setAttribute('aria-expanded', false);
-    bodyScrollLock.enableBodyScroll(document.body);
+    menuIcon.setAttribute('href', './images/svg-sprite.svg#ham-menu-icon');
+    document.body.classList.remove('no-scroll'); // дозволяємо прокручування
   };
   mediaQuery.addListener(handleOrientationChange);
 
@@ -31,7 +33,6 @@ export function MobileMenu() {
     toggleMenu,
     destroy: () => {
       openMenuBtn.removeEventListener('click', toggleMenu);
-      closeMenuBtn.removeEventListener('click', toggleMenu);
       mediaQuery.removeListener(handleOrientationChange);
     },
   };
