5f2cc2009c71b26dc77daa51a2a2aa3a8042fae2
Stanislav-Zhuk <steys.ua@gmail.com>
2023-04-17 15:23:38 +0300

    Update modal-pop-up-template.js

--- src/js/modal-pop-up-template.js
+++ src/js/modal-pop-up-template.js
@@ -1,18 +1,29 @@
+import amazon1x from '../images/BookStoreIcons/amazon.png';
+import amazon2x from '../images/BookStoreIcons/amazon-2x.png';
+import appleBooks1x from '../images/BookStoreIcons/apple-books.png';
+import appleBooks2x from '../images/BookStoreIcons/apple-books-2x.png';
+import bookshop1x from '../images/BookStoreIcons/book-shop.png';
+import bookshop2x from '../images/BookStoreIcons/book-shop-2x.png';
+import closeBtnIcon from '../images/svg-sprite.svg';
+
+
 export function createModalPopUpCard(book) {
   const { _id, title, author, book_image, description, buy_links } = book;
   const urls = buy_links
-  .filter(buyLink => ["Amazon", "Apple Books", "Bookshop"].includes(buyLink.name))
-  .map(buyLink => buyLink.url);
-  return `<div id=${_id} data-fancybox="modal" class="modal" style="display:none">
+    .filter(buyLink =>
+      ['Amazon', 'Apple Books', 'Bookshop'].includes(buyLink.name)
+    )
+    .map(buyLink => buyLink.url);
+  return `<div id="modal" data-fancybox="modal" class="modal" style="display:none">
     <div class="modal__pop-up">
       <button class="pop-up__btn-close" type="button">
         <svg class="btn-close-icon" width="32" height="32">
-          <use href="./images/svg-sprite.svg#x-close-icon"></use>
+          <use href="${closeBtnIcon}#x-close-icon"></use>
         </svg>
       </button>
       <div class="pop-up__book">
         <div class="pop-up__book-container">
-          <img class="book__image" src="${book_image}" alt="">
+          <img class="book__image" src="${book_image}" alt="" >
           <div>
             <div>
               <h2 class="book__title info-item__title">${title}</h2>
@@ -22,9 +33,9 @@ export function createModalPopUpCard(book) {
             <ul class="book__list">
               <li class="book__item">
                 <a class="book__buy-links" href="${urls[0]}" target="_blank" rel="noopener noreferrer" aria-label="Amazon">
-                  <picture class="buy-links__icon">
-                    <source srcset="./images/BookStoreIcons/amazon.png 1x, ./images/BookStoreIcons/amazon-2x.png 2x">
-                    <img src="./images/BookStoreIcons/amazon.png" alt="Amazon">
+                  <picture class="buy-links__icon amazon">
+                    <source srcset="${amazon1x} 1x, ${amazon2x} 2x">
+                    <img src="${amazon2x}" alt="Amazon">
                   </picture>
                 </a>
               </li>
@@ -32,8 +43,8 @@ export function createModalPopUpCard(book) {
                 <a class="book__buy-links" href="${urls[1]}" target="_blank" rel="noopener noreferrer" aria-label="Apple Books">
                   <picture class="buy-links__icon">
                     <source
-                      srcset="./images/BookStoreIcons/apple-books.png 1x, ./images/BookStoreIcons/apple-books-2x.png 2x">
-                    <img src="./images/BookStoreIcons/apple-books.png" alt="Apple Books">
+                      srcset="${appleBooks1x} 1x, ${appleBooks2x} 2x">
+                    <img src="${appleBooks2x}" alt="Apple Books" data="${_id}">
                   </picture>
                 </a>
               </li>
@@ -41,8 +52,8 @@ export function createModalPopUpCard(book) {
                 <a class="book__buy-links" href="${urls[2]}" target="_blank" rel="noopener noreferrer" aria-label="Bookshop">
                   <picture class="buy-links__icon">
                     <source
-                      srcset="./images/BookStoreIcons/book-shop.png 1x, ./images/BookStoreIcons/book-shop-2x.png 2x">
-                    <img src="./images/BookStoreIcons/book-shop.png" alt="Bookshop">
+                      srcset="${bookshop1x} 1x, ${bookshop2x} 2x">
+                    <img src="${bookshop2x}" alt="Bookshop">
                   </picture>
                 </a>
               </li>
