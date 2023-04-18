35d709173388c70a4de9896fc423d520c7ca582c
Serhii Naboka <drifterzzz@gmail.com>
2023-04-18 21:28:59 +0300

    in proc

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,23 +1,47 @@
-import Swiper, { Navigation, Pagination, Scrollbar } from 'swiper';
-// import 'swiper/css';
+import Swiper, {
+  Keyboard,
+  Navigation,
+  Pagination,
+  Scrollbar,
+  Keyboard,
+  Grid,
+  Zoom,
+  Controller,
+  A11y,
+  Thumbs,
+} from 'swiper';
 
 const swiper = new Swiper('.swiper', {
-  modules: [Navigation, Pagination, Scrollbar],
+  modules: [
+    Navigation,
+    Pagination,
+    Scrollbar,
+    Keyboard,
+    Grid,
+    Zoom,
+    Controller,
+    A11y,
+    Thumbs,
+  ],
+
   loop: true,
   direction: 'vertical',
-  // navigation: {
-  //   nextEl: '.support__load-more',
-  // },
-  // keyboard: {
-  //   enabled: true,
-  //   onlyInviewPort: true,
-  //   pageUpDown: true,
-  // },
-  // simulateTouch: false,
-  // slidesPerGroup: 1,
-  // spaceBetween: 20,
-  // speed: 250,
-  // slidesPerView: 6,
+
+  navigation: {
+    nextEl: '.support__load-more',
+  },
+
+  keyboard: {
+    enabled: true,
+    onlyInviewPort: true,
+    pageUpDown: true,
+  },
+
+  simulateTouch: false,
+  slidesPerGroup: 1,
+  spaceBetween: 20,
+  speed: 250,
+  slidesPerView: 5,
 });
 
 // breakpoints: {
