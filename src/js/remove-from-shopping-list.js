d4f154b5894e930d11c9423964542f002767ef3c
NovicovMykhailo <mikenovicov@gmail.com>
2023-04-19 15:18:51 +0300

    Update remove-from-shopping-list.js

--- src/js/remove-from-shopping-list.js
+++ src/js/remove-from-shopping-list.js
@@ -3,7 +3,7 @@ export default function removeFromLocalStorage(bookId) {
   try {
     books.map((b, i) => {
       if (b.title === bookId.title) {
-        console.dir('removeFromLocalStorage -> books[i]', books[i])
+        // console.dir('removeFromLocalStorage -> books[i]', books[i])
         books.splice(i, 1);
         localStorage.setItem('shopping-list', JSON.stringify(books));
         return;
