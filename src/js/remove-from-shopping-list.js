a13edd020e1b5d704fbedebff47afad7b1af2037
NovicovMykhailo <mikenovicov@gmail.com>
2023-04-19 15:17:05 +0300

    fix remove function

--- src/js/remove-from-shopping-list.js
+++ src/js/remove-from-shopping-list.js
@@ -1,11 +1,15 @@
 import { books } from './shopping-list__books';
 export default function removeFromLocalStorage(bookId) {
   try {
-    const arrayOfId = [];
-    books.map(book => arrayOfId.push(book.id));
-
-    books.splice(books[arrayOfId.indexOf(bookId)], 1);
-    localStorage.setItem('shopping-list', JSON.stringify(books));
+    books.map((b, i) => {
+      if (b.title === bookId.title) {
+        console.dir('removeFromLocalStorage -> books[i]', books[i])
+        books.splice(i, 1);
+        localStorage.setItem('shopping-list', JSON.stringify(books));
+        return;
+      }    
+    }
+    );
   } catch (error) {
     console.log(error);
   }
