69a6b337ec66a62053a8b8ce1a7ab91d04c94c65
NovicovMykhailo <mikenovicov@gmail.com>
2023-04-19 04:49:37 +0300

    add render card function

--- src/js/shopping-list.js
+++ src/js/shopping-list.js
@@ -5,13 +5,7 @@ import { renderPaginationBtn, currentPage } from '../js/shopping-list__paginatio
 import { paginationBtnEl } from '../js/shopping-list__pagination';
 import { books } from './shopping-list__books';
 
-import amazon1x from '../images/BookStoreIcons/amazon.png';
-import amazon2x from '../images/BookStoreIcons/amazon-2x.png';
-import appleBooks1x from '../images/BookStoreIcons/apple-books.png';
-import appleBooks2x from '../images/BookStoreIcons/apple-books-2x.png';
-import bookshop1x from '../images/BookStoreIcons/book-shop.png';
-import bookshop2x from '../images/BookStoreIcons/book-shop-2x.png';
-import spriteSvgEls from '../images/svg-sprite.svg';
+
 
 
 const addBookBtnEl = document.querySelector('[data-add]');
@@ -36,189 +30,6 @@ const getListObjectsOfPage = numberPage => {
   return arrayOfBooks;
 }
 
-const isAvailableDescription = (description) => {
-  if (description === '') {
-    return "Sorry, we can't find appropriate description :("
-  }
-
-  return description;
-}
-
-
-//================================
-// const oldMurkupPictures = `<li>
-// <div class="shopping-list__card">
-//   <img src="${book_image}" alt="book cover" class='shopping-list__image' />
-//   <div class="shopping-list__head-part">
-//     <h2 class='shopping-list__title-card'>${title}</h2>
-//       <p class="shopping-list__sub-title">Sub-title</p>
-//       <p class="shopping-list__description">${isAvailableDescription(description)}
-//       </p>
-//       <div class='shopping-list__card-container'>
-//         <h3 class='shopping-list__author'>${author}</h3>
-//         <ul class="shopping-list__links">
-//           <li class="shopping-list__link-item">
-//               <a href="${buy_links[0].url}" class="shopping-list__link" target='blank'>
-//                   <img src="../images/BookStoreIcons/amazon.png" width="48" alt="platform-icon" class="shopping-list__link-icon">
-//               </a>
-//           </li>
-//           <li class="shopping-list__link-item">
-//               <a href="${buy_links[1].url}" class="shopping-list__link" target='blank'>
-//                   <img src="../images/BookStoreIcons/apple-books.png" width="28" alt="platform-icon" class="shopping-list__link-icon">
-//               </a>
-//           </li>
-//           <li class="shopping-list__link-item">
-//               <a href="${buy_links[4].url}" class="shopping-list__link" target='blank'>
-//                   <img src="../images/BookStoreIcons/book-shop.png" width="32" alt="platform-icon" class="shopping-list__link-icon">
-//               </a>
-//           </li>
-//         </ul>
-//       </div>
-//   </div>
-//   <button class="btn">
-//     <svg class="trash-icon" width="16" height="16">
-//       <use href="${spriteSvgEls}#trash-icon"></use>
-//     </svg>
-//   </button>
-// </div>
-// </li>`;
-// ==========================================
-
-const murkupForTabketAndDesktop = ({ author, title, book_image, buy_links, description }) => `<li>
-<div class="shopping-list__card">
-  <img src="${book_image}" alt="book cover" class='shopping-list__image' />
-  <div class="shopping-list__head-part">
-    <h2 class='shopping-list__title-card'>${title}</h2>
-      <p class="shopping-list__sub-title">Sub-title</p>
-      <p class="shopping-list__description">${isAvailableDescription(description)}
-      </p>
-      <div class='shopping-list__card-container'>
-        <h3 class='shopping-list__author'>${author}</h3>
-        <ul class="shopping-list__links">
-          <li class="shopping-list__link-item">
-              <a href="${buy_links[0].url}" class="shopping-list__link" target='blank'>
-                  <img class="buy-links__icon" src="${amazon1x}" width="48" alt="platform-icon" class="shopping-list__link-icon">
-              </a>
-          </li>
-          <li class="shopping-list__link-item">
-              <a href="${buy_links[1].url}" class="shopping-list__link" target='blank'>
-                  <img class="buy-links__icon" src="${appleBooks1x}" width="28" alt="platform-icon" class="shopping-list__link-icon">
-              </a>
-          </li>
-          <li class="shopping-list__link-item">
-              <a href="${buy_links[4].url}" class="shopping-list__link" target='blank'>
-                  <img class="buy-links__icon" src="${bookshop1x}" width="32" alt="platform-icon" class="shopping-list__link-icon">
-              </a>
-          </li>
-        </ul>
-      </div>
-  </div>
-  <button class="btn">
-    <svg class="trash-icon" width="16" height="16">
-      <use href="${spriteSvgEls}#trash-icon"></use>
-    </svg>
-  </button>
-</div>
-</li>`;
-
-// const newM =  `<li>
-// <div class="shopping-list__card">
-//   <img src="${book_image}" alt="book cover" class='shopping-list__image' />
-//   <div class="shopping-list__head-part">
-//     <h2 class='shopping-list__title-card'>${title}</h2>
-//       <p class="shopping-list__sub-title">Sub-title</p>
-//       <p class="shopping-list__description">${isAvailableDescription(description)}
-//       </p>
-//       <div class='shopping-list__card-container'>
-//         <h3 class='shopping-list__author'>${author}</h3>
-//         <ul class="shopping-list__links">
-//           <li class="shopping-list__link-item">
-//             <a href="${buy_links[0].url}" target="_blank" rel="noopener noreferrer" aria-label="Amazon">
-//               <picture>
-//                 <source srcset="${amazon1x} 1x, ${amazon2x} 2x">
-//                 <img src="${amazon2x}" alt="Amazon">
-//               </picture>
-//             </a>
-//           </li>
-//           <li class="shopping-list__link-item">
-//             <a href="${buy_links[1].url}" target="_blank" rel="noopener noreferrer" aria-label="Apple Books">
-//               <picture>
-//                <source
-//                   srcset="${appleBooks1x} 1x, ${appleBooks2x} 2x">
-//                 <img src="${appleBooks2x}" alt="Apple Books" data="${_id}">
-//               </picture>
-//             </a>
-//           </li>
-//           <li class="shopping-list__link-item">
-//             <a href="${buy_links[4].url}" target="_blank" rel="noopener noreferrer" aria-label="Bookshop">
-//               <picture>
-//                 <source
-//                   srcset="${bookshop1x} 1x, ${bookshop2x} 2x">
-//                 <img src="${bookshop2x}" alt="Bookshop">
-//               </picture>
-//             </a>
-//           </li>
-//         </ul>
-//       </div>
-//   </div>
-//   <button class="btn">
-//     <svg class="trash-icon" width="16" height="16">
-//       <use href="${spriteSvgEls}#trash-icon"></use>
-//     </svg>
-//   </button>
-// </div>
-// </li>`;
-
-
-const murkupForMobile = ({ author, title, book_image, buy_links, description }) => `<li>
-<div class="shopping-list__card">
-  <div class="shopping-list__sub-card">
-    <div class="shopping-list__haed-part__mobile">
-      <img src="${book_image}" alt="book cover" class="shopping-list__image">
-    </div>
-    <div>
-      <h2 class="shopping-list__title-card">${title}</h2>
-      <p class="shopping-list__sub-title">Sub-title</p>
-      <ul class="shopping-list__links">
-        <li class="shopping-list__link-item">
-          <a href="${buy_links[0].url}" target="_blank" rel="noopener noreferrer" aria-label="Amazon">
-            <picture>
-              <source srcset="${amazon1x} 1x, ${amazon2x} 2x">
-              <img src="${amazon2x}" alt="Amazon">
-            </picture>
-          </a>
-        </li>
-        <li class="shopping-list__link-item">
-          <a href="${buy_links[1].url}" target="_blank" rel="noopener noreferrer" aria-label="Apple Books">
-            <picture>
-             <source
-                srcset="${appleBooks1x} 1x, ${appleBooks2x} 2x">
-              <img src="${appleBooks2x}" alt="Apple Books" data="${_id}">
-            </picture>
-          </a>
-        </li>
-        <li class="shopping-list__link-item">
-          <a href="${buy_links[4].url}" target="_blank" rel="noopener noreferrer" aria-label="Bookshop">
-            <picture>
-              <source
-                srcset="${bookshop1x} 1x, ${bookshop2x} 2x">
-              <img src="${bookshop2x}" alt="Bookshop">
-            </picture>
-          </a>
-        </li>
-      </ul>
-    </div>
-  </div>
-  <p class="shopping-list__author">${author}</p>
-  <p class="shopping-list__description">${isAvailableDescription(description)}</p>
-  <button class="btn">
-    <svg class="trash-icon" width="16" height="16">
-      <use href="${spriteSvgEls}#trash-icon"></use>
-    </svg>
-  </button>
-</div>
-</li>`;
-
 const getAppropriateMurkup = (book) => {
   if (window.screen.width < 768) {
     return murkupForMobile(book); // !!! может быть ошибка с передачей строки
@@ -238,8 +49,6 @@ export const murkupShoppingList = numberPage => {
   return arrayOfMurkup.join('');
 
 }
-
-
 
 const checkCountOfPages = () => {
   return Math.ceil(books.length / 3);
