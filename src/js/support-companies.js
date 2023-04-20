c0b04e0a253b97dedb4605e4328e38995cf18033
HalynaSilina <halynasilina@gmail.com>
2023-04-20 15:07:10 +0300

    fix styles

--- src/js/support-companies.js
+++ src/js/support-companies.js
@@ -3,16 +3,17 @@ import { supportCompanies } from './support-data.js';
 const companiesList = document.querySelector('.support-companies');
 
 function createSupportList() {
+  
   let counter = 0;
-
   const markup = supportCompanies
     .map(({ url, img, img2, title }) => {
       counter += 1;
-      return `<li class="company__item swiper-slide"> ${leadingZeros(counter)}
-    <a class="company__link" href="${url}" target="_blank" rel="noopener noreferrer">
-      <img class="company__img" src="${img2}" srcset="${img} 1x, ${img2} 2x" alt="${title}"  loading="lazy" />
-     </a>
-    </li>`;
+      return `<li class="company__item swiper-slide">
+      <span class="numbers">${leadingZeros(counter)}</span>
+      <a class="company__link" href="${url}" target="_blank" rel="noopener noreferrer">
+      <img class="company__img" src="${img2}" srcset="${img} 1x, ${img2} 2x" alt="${title}" loading="lazy"/>
+      </a>
+      </li>`;
     })
     .join('');
 
