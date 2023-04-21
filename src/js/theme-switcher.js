7d940d9b9a8824891ad36cf33fb6a3a7cf65fb84
NovicovMykhailo <mikenovicov@gmail.com>
2023-04-21 22:31:37 +0300

    match media queries for dark theme prefer

--- src/js/theme-switcher.js
+++ src/js/theme-switcher.js
@@ -16,3 +16,22 @@ themeSwitch.addEventListener('change', function() {
   }
 });
 
+// window
+//   .matchMedia('(prefers-color-scheme: dark)')
+//   .addEventListener('change', () => {
+
+//   });
+
+  // Check to see if Media-Queries are supported
+if (window.matchMedia) {
+  // Check if the dark-mode Media-Query matches
+  if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    // Dark
+    body.classList.add('dark-theme');
+    themeSwitch.checked = true;
+  } else {
+    // Light
+    localStorage.setItem('theme', 'light');
+    body.classList.remove('dark-theme');
+  }
+} 
