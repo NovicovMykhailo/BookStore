6d0d009fefd425598568e498aa64f75cbd478c59
Michael Novicov <114993840+NovicovMykhailo@users.noreply.github.com>
2023-04-25 13:36:47 +0300

    Update support-data.js

--- src/js/support-data.js
+++ src/js/support-data.js
@@ -17,59 +17,61 @@ import world2x from '../images/SupportUkraineCompanies/word-vision-2x.png';
 import prytula from '../images/SupportUkraineCompanies/prytula.png';
 import prytula2x from '../images/SupportUkraineCompanies/prytula-2x.png';
 
-export default supportCompanies = [
-  {
-    title: 'Save the Children',
-    url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis ',
-    img: save,
-    img2: save2x,
-  },
-  {
-    title: 'Project HOPE',
-    url: 'https://www.projecthope.org/country/ukraine/ ',
-    img: hope,
-    img2: hope2x,
-  },
-  {
-    title: 'UNITED24',
-    url: 'https://u24.gov.ua/uk ',
-    img: united,
-    img2: united2x,
-  },
-  {
-    title: 'International Medical Corps',
-    url: 'https://internationalmedicalcorps.org/country/ukraine/ ',
-    img: corps,
-    img2: corps2x,
-  },
-  {
-    title: 'Medicins Sans Frontieres',
-    url: 'https://www.msf.org/ukraine ',
-    img: medsans,
-    img2: medsans2x,
-  },
-  {
-    title: 'RAZOM',
-    url: 'https://www.razomforukraine.org/ ',
-    img: razom,
-    img2: razom2x,
-  },
-  {
-    title: 'Action against hunger',
-    url: 'https://www.actionagainsthunger.org/location/europe/ukraine/ ',
-    img: action,
-    img2: action2x,
-  },
-  {
-    title: 'World vision',
-    url: 'https://www.wvi.org/emergencies/ukraine ',
-    img: world,
-    img2: world2x,
-  },
-  {
-    title: 'Serhiy Prytula Charity Foundation',
-    url: 'https://prytulafoundation.org/en ',
-    img: prytula,
-    img2: prytula2x,
-  },
-];
+export const supportCompanies = 
+  [
+    {
+      title: 'Save the Children',
+      url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis ',
+      img: save,
+      img2: save2x,
+    },
+    {
+      title: 'Project HOPE',
+      url: 'https://www.projecthope.org/country/ukraine/ ',
+      img: hope,
+      img2: hope2x,
+    },
+    {
+      title: 'UNITED24',
+      url: 'https://u24.gov.ua/uk ',
+      img: united,
+      img2: united2x,
+    },
+    {
+      title: 'International Medical Corps',
+      url: 'https://internationalmedicalcorps.org/country/ukraine/ ',
+      img: corps,
+      img2: corps2x,
+    },
+    {
+      title: 'Medicins Sans Frontieres',
+      url: 'https://www.msf.org/ukraine ',
+      img: medsans,
+      img2: medsans2x,
+    },
+    {
+      title: 'RAZOM',
+      url: 'https://www.razomforukraine.org/ ',
+      img: razom,
+      img2: razom2x,
+    },
+    {
+      title: 'Action against hunger',
+      url: 'https://www.actionagainsthunger.org/location/europe/ukraine/ ',
+      img: action,
+      img2: action2x,
+    },
+    {
+      title: 'World vision',
+      url: 'https://www.wvi.org/emergencies/ukraine ',
+      img: world,
+      img2: world2x,
+    },
+    {
+      title: 'Serhiy Prytula Charity Foundation',
+      url: 'https://prytulafoundation.org/en ',
+      img: prytula,
+      img2: prytula2x,
+    },
+  ];
+
